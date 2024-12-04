4d6eb9d177874fd50217b60173e41e6228fa22fd
Kozora ALT <roidavmunkat@gmail.com>
2024-12-04 21:31:48 +0100

    added option of check a list

--- src/NewVersions/ListCard/ListCard.jsx
+++ src/NewVersions/ListCard/ListCard.jsx
@@ -6,6 +6,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import uniqid from 'uniqid';
 
+// The cn utility function (short for "classnames") is a helper function that combines clsx and tailwind-merge to handle class name concatenation and conflicts in Tailwind CSS
+
 const ListCard = ({item, todos, setTodos}) => {
 
     function handleDelete(item){
@@ -15,23 +17,24 @@ const ListCard = ({item, todos, setTodos}) => {
     }
 
     function handleClick(name){
-        const newArray = todos.map((todo) =>todo.name === name? {...todo,done:!todo.done}: todo)
 
-        setTodos(newArray)
-        console.log(todos)
+        setTodos( todos.map((todo) =>todo.name === name? {...todo,done:!todo.done}: todo))
+
     }
+
+    const completed = item.done? "line-through opacity-50 text-gray-500": ""
 
     return(
         <>
         <section className="bg-white rounded-lg shadow-md p-4 mb-4">
             <div className="flex items-center justify-between"></div>
-            <input  className="float-left m-2 mr-5 size-5" type="checkbox" />
+            <input  className="float-left m-2 mr-5 size-5" comppleted={completed} onClick={() => handleClick(item.name)} type="checkbox" />
             {/* onClick={() => onToggle(task.id)} */}
             {/* As if i used a "IF" */}
 
             <div>
             <h3 className="text-gray-900">
-            <span onClick={() => handleClick(item.name)}>{item.name}</span>
+            <span className={completed} onClick={() => handleClick(item.name)}>{item.name}</span>
             </h3>
             {/* className={`text-lg font-medium ${task.completed ? 'line-through text-gray-500' : 'text-gray-900'}`} */}
             {/* {task.title} */}
